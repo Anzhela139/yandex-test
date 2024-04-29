aa26b72a3789622fb897302f3f9dace1bb4a3ab7
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2024-04-29 18:41:38 +0400

    mobile and fixes

--- assets/scripts/components/carousel.js
+++ assets/scripts/components/carousel.js
@@ -1,40 +1,95 @@
+function minmax(val, min, max) {
+    return Math.min(Math.max(val, min), max);
+}
+
 class Carousel {
-    constructor(carouselWrapper, carousel, controls, textElem) {
+    constructor(carouselWrapper, carousel, controls, textElem, cssTransition, paddingTransition, isDots) {
         this.carouselWrapper = carouselWrapper;
         this.carousel = carousel;
         this.controls = controls;
         this.textElem = textElem;
+        this.cssTransition = cssTransition;
+        this.paddingTransition = paddingTransition;
+        this.isDots = isDots;
     }
 
     init() {
-        console.log(this)
         this.controls.addEventListener('click', this.handleControls.bind(this));
+        this.setCarouselTransition(1);
+        this.handleResize();
+    }
+
+    handleResize() {
+        if (window.scrollbarWidth <= 640 && !this.isDots) {
+            this.textElem.innerHTML = '1 / 6'
+        }
     }
 
     handleControls(event) {
         const btn = event.target.closest('.btn');
-        console.log(btn)
-        if (btn.dataset.direction === 'right') {
-            this.changeNumberText(true);
+        if (this.isDots) {
+            this.changeDots(btn, btn.closest('[data-direction]').dataset.direction === 'right');
         } else {
-            this.changeNumberText(false);
+            this.changeNumberText(btn, btn.closest('[data-direction]').dataset.direction === 'right');
         }
     }
 
-    changeNumberText(isForward = true) {
+    getNumText(arr, length, isForward) {
+        const numSlides = window.scrollbarWidth <= 640 ? 1 : 3;
+        const numArr = arr.map((el) => parseInt(el))
+        console.log(numArr, numSlides, length)
+        return [
+            minmax(isForward ? numArr[0] + numSlides : numArr[0] - numSlides, 1, length), 
+            length
+        ];
+    }
+
+    setDisabled(current, cardsLength) {
+        console.log(current, cardsLength, current === 1, (window.scrollbarWidth > 640 && current === 3))
+        if (current === 1 || (window.scrollbarWidth > 640 && current === 3)) {
+            this.controls.querySelector('.control__left').classList.add('disabled');
+            this.controls.querySelector('.control__right').classList.remove('disabled');
+        } else if (current === cardsLength) {
+            this.controls.querySelector('.control__left').classList.remove('disabled');
+            this.controls.querySelector('.control__right').classList.add('disabled');
+        } else {
+            this.controls.querySelector('.control__left').classList.remove('disabled');
+            this.controls.querySelector('.control__right').classList.remove('disabled');
+        }
+    }
+
+    setCarouselTransition(current) {
+        document.documentElement.style.setProperty(this.cssTransition, -((window.scrollbarWidth - this.paddingTransition) * (current - 1)) + "px");
+    }
+
+    changeDots(btn, isForward) {
+        const oldCurrentDom = this.controls.querySelector('.current');
+        const oldCurrent = parseInt(oldCurrentDom.dataset.dot);
+        if ((oldCurrent === 5 && isForward) || (oldCurrent === 1 && !isForward)) return
+        const current = isForward ? oldCurrent + 1 : oldCurrent - 1;
+
+        oldCurrentDom.classList.remove('current');
+        this.controls.querySelector(`[data-dot="${current}"]`).classList.add('current');
+        this.setDisabled(current, 5);
+        this.setCarouselTransition(current)
+    }
+
+    changeNumberText(btn, isForward = true) {
         const numText = this.textElem.innerText.split(' / ');
-        const cardsLength = this.carousel.querySelectorAll('.members__card').length;
+        const cardsLength = this.carousel.querySelectorAll('.carousel-card').length;
+        const newText = this.getNumText(numText, cardsLength, isForward);
+        this.textElem.innerHTML = newText.join(' / ')
+        this.setDisabled(newText[0], cardsLength)
+        this.setCarouselTransition(newText[0])
         if (isForward) {
-            this.textElem.innerHTML = numText[1] === String(cardsLength) ? this.textElem.innerText : `${parseInt(numText[0]) + 3} / ${parseInt(numText[1]) + 3}`;
+
             this.carousel.classList.remove('backward');
             this.carousel.classList.add('forward');
         } else {
-            this.textElem.innerHTML = numText[0] === '1' ? this.textElem.innerText : `${parseInt(numText[0]) - 3} / ${parseInt(numText[1]) - 3}`;
             this.carousel.classList.remove('forward');
             this.carousel.classList.add('backward');
         }
 
-        console.log(this.textElem.innerHTML)
     }
 }
 
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,11 +1,59 @@
 import Carousel from './components/carousel.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const carousel = new Carousel(
-        document.querySelector('.carousel-wrapper'), 
-        document.querySelector('.members__carousel'), 
-        document.querySelector('.carousel-controls'), 
-        document.querySelector('.carousel-controls__text')
-    );
-    carousel.init();
-});
+class Main {
+    constructor() {
+        this.membersCarousel = null;
+        this.stagesCarousel = null;
+    }
+
+    init() {
+        this.setScrollbarWidth();
+        window.addEventListener('resize', this.handleResize.bind(this));
+        document.addEventListener('DOMContentLoaded', this.initCarousel.bind(this));
+    }
+
+    handleResize() {
+        this.setScrollbarWidth();
+        this.initCarousel();
+    }
+
+    initCarousel() {
+        if (!this.membersCarousel) {
+            
+            this.membersCarousel = new Carousel(
+                document.querySelector('.stages .carousel-wrapper'), 
+                document.querySelector('.stages .stages__grid'), 
+                document.querySelector('.stages .carousel-controls'), 
+                document.querySelector('.stages .carousel-controls__text'),
+                '--carousel-stages-transition',
+                0,
+                true
+            );
+            this.membersCarousel.init();
+        } else {
+            this.membersCarousel.handleResize()
+        }
+
+        if (!this.stagesCarousel) {
+            this.stagesCarousel = new Carousel(
+                document.querySelector('.members .carousel-wrapper'), 
+                document.querySelector('.members .members__carousel'), 
+                document.querySelector('.members .carousel-controls'), 
+                document.querySelector('.members .carousel-controls__text'),
+                '--carousel-members-transition',
+                20,
+                false
+            );
+            this.stagesCarousel.init();
+        }
+    }
+
+    setScrollbarWidth() {
+        this.scrollbarWidth = window.innerWidth - (window.innerWidth - document.documentElement.clientWidth);
+        window.scrollbarWidth = this.scrollbarWidth;
+        document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - (window.innerWidth - document.documentElement.clientWidth)) + "px");
+    }
+}
+
+const main = new Main();
+main.init();
